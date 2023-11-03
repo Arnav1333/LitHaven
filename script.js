bfc11d0604e16d5f4ecdc1101fc3946a1e1fb0b1
Arnav <pawar.arnav1333@gmail.com>
2023-11-04 02:23:28 +0530

    smoothscroll and contact page plus ham button

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark");
+}
+function toggleMenu() {
+    let nav = document.querySelector('.nav-link');
+    nav.classList.toggle('active');
+}
